2c75838965d2d7a04bcfc7cc723f1a90f463c994
Tixerz <petete1296@gamil.com>
2023-08-23 01:14:17 -0400

    bug fixes

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 from cryptography.fernet import Fernet
- 
+
 path_to_key = input("Enter the path to the key for encryption and decryption:")
-path_to_file = input("Enter the path to the file you want to ecrypt and decrypt:")
+path_to_file = input("Enter the path to the file you want to encrypt and decrypt:")
 enorde = input("\n=======\n[E/D]:")
-with open(path_to_key , "r") as f :
+with open(path_to_key, "r") as f:
     key = f.read()
-if enorde == "e" or "E" :
-    with open(path_to_file , "r") as file:
-        data= file.read()
-    byte_data= data.encode()
+
+if enorde.lower() == "e":
+    with open(path_to_file, "r") as file:
+        data = file.read()
+    byte_data = data.encode()
     cryptor = Fernet(key)
     encrypted = cryptor.encrypt(byte_data)
-    with open("ecrypted_item.enc" , "wb") as enc :
+    with open("encrypted_item.enc", "wb") as enc:
         enc.write(encrypted)
-elif enorde == "d" or "D"  :
-    with open(path_to_file , "r") as file:
-        data= file.read()
-    byte_data= data.encode()
+
+elif enorde.lower() == "d":
+    with open(path_to_file, "rb") as file:
+        data = file.read()
     cryptor = Fernet(key)
-    encrypted = cryptor.encrypt(byte_data)
-    with open("decrypted_item.enc" , "wb") as enc :
-        enc.write(encrypted)
-else :
-    pass
+    decrypted = cryptor.decrypt(data)
+    with open("decrypted_item.enc", "wb") as enc:
+        enc.write(decrypted)
+
+else:
+    print("Something went wrong.")
